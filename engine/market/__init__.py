50f9f3b544898802e82d310533099ccd598d3dd3
super-curve-hub <motoki.kajitani@gmail.com>
2026-06-26 10:40:39 +0900

    Update __init__.py

--- engine/market/__init__.py
+++ engine/market/__init__.py
@@ -4,10 +4,12 @@ Market Layer
 
 from .yahoo import (
     fetch_market,
+    fetch_market_data,
     safe_download,
 )
 
 __all__ = [
     "fetch_market",
+    "fetch_market_data",
     "safe_download",
 ]
